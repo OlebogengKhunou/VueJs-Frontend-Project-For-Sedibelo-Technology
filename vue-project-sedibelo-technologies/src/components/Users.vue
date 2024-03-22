a49a271a20c8c882d41836b6ae317053905b1b5e
Olebogeng Khunou (OLEKHU897) <olebigeng@gmail.com>
2024-03-22 13:57:12 +0200

    created Users.vue component
    Added code to fetch users' data from users json file and created a datatable to store and view that data

---
+++ vue-project-sedibelo-technologies/src/components/Users.vue
@@ -0,0 +1,34 @@
+<template>
+<p v-for="user in users" :key="users.indexOf(user)">{{user.name}}</p>
+<table id="myTable" width='400' height='400'>
+</table>
+</template>
+
+<script >
+import usersData from './jsons/users.json'
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-responsive-dt';
+
+console.log(usersData)
+export default{
+  data(){
+     return { users: usersData }
+  },
+  mounted(){
+ 
+let dataTable = new DataTable('#myTable', {
+  responsive: true,
+  data: usersData,
+   columns: [
+        { data: 'name' },
+        { data: 'surname' },
+        { data: 'designation' },
+        { data: 'department' }
+    ]
+    // config options...
+});
+
+dataTable;
+  }
+}
+</script>
