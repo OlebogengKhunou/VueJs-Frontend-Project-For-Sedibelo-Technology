5ef047564067241b82e9e798f98be432c656c572
Olebogeng Khunou (OLEKHU897) <olebigeng@gmail.com>
2024-03-25 17:12:49 +0200

    changed color on pie chart

--- vue-project-sedibelo-technologies/src/components/PieChart.vue
+++ vue-project-sedibelo-technologies/src/components/PieChart.vue
@@ -32,7 +32,7 @@ export default {
         label: 'NumInstalls',
         data: numInstalls,
         backgroundColor: [
-          'rgb(255, 99, 132)',
+          'rgb(255, 0, 0)',
           'rgb(54, 162, 235)',
           'rgb(255, 205, 86)',
           'rgb(255, 99, 132)',
